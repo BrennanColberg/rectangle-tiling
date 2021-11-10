a8a6c6fe2a88a3a47d2fd87d6a14f2ca990ab33b
Brennan Colberg <brennan.colberg@gmail.com>
2021-11-10 13:56:05 -0800

    expensive but functional algorithm

--- algorithm.js
+++ algorithm.js
@@ -8,4 +8,25 @@ function v2(longer, shorter, count) {
 	return longer / count;
 }
 
-const algorithm = v2;
+function expensive(longer, shorter, count) {
+	// `shorter` is the largest possible size, so we count
+	// the size down from there until all the boxes fit for one!
+	for (let size = shorter; size > 0; size--) {
+		// simulate putting in `count` boxes
+		let x = 0; // same axis as `longer`
+		let y = 0; // same axis as `shorter`
+		for (let i = 0; i < count; i++) {
+			x += size;
+			// wrap around whenever we overflow on the long side
+			if (x > longer) {
+				x = size;
+				y += size;
+			}
+		}
+		// if we *aren't* overflowing on the short side, this is
+		// the biggest size that fits, so we should return it!
+		if (y + size <= shorter) return size;
+	}
+}
+
+const algorithm = expensive;
