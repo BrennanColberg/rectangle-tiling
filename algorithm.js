56add0d1c75ae3ab64071a3df49476c8dde54640
Brennan Colberg <brennan.colberg@gmail.com>
2021-11-10 13:59:32 -0800

    factor out overflowCheck

--- algorithm.js
+++ algorithm.js
@@ -8,24 +8,29 @@ function v2(longer, shorter, count) {
 	return longer / count;
 }
 
+// helper for later algorithms
+function overflowCheck(longer, shorter, count, size) {
+	// simulate putting in `count` boxes
+	let x = 0; // same axis as `longer`
+	let y = 0; // same axis as `shorter`
+	for (let i = 0; i < count; i++) {
+		x += size;
+		// wrap around whenever we overflow on the long side
+		if (x > longer) {
+			x = size;
+			y += size;
+		}
+	}
+	// if we *aren't* overflowing on the short side, this is
+	// the biggest size that fits, so we should return it!
+	return y + size > shorter;
+}
+
 function expensive(longer, shorter, count) {
 	// `shorter` is the largest possible size, so we count
 	// the size down from there until all the boxes fit for one!
 	for (let size = shorter; size > 0; size--) {
-		// simulate putting in `count` boxes
-		let x = 0; // same axis as `longer`
-		let y = 0; // same axis as `shorter`
-		for (let i = 0; i < count; i++) {
-			x += size;
-			// wrap around whenever we overflow on the long side
-			if (x > longer) {
-				x = size;
-				y += size;
-			}
-		}
-		// if we *aren't* overflowing on the short side, this is
-		// the biggest size that fits, so we should return it!
-		if (y + size <= shorter) return size;
+		if (!overflowCheck(longer, shorter, count, size)) return size;
 	}
 }
 
