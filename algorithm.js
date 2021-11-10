fb3a507928e2a8dad2a4f27f6069ce0dd3ed87f9
Brennan Colberg <brennan.colberg@gmail.com>
2021-11-10 12:37:39 -0800

    updateTiling

---
+++ algorithm.js
@@ -0,0 +1,3 @@
+function computeSizeOfEachVideo({ width, height }, count) {
+	return 20;
+}
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 let boxes = 4;
 
 function updateTiling() {
-	renderBoxes();
+	const area = document.getElementById("area");
+	document.documentElement.style.setProperty(
+		"--size",
+		`${computeSizeOfEachVideo(area.clientWidth, area.clientHeight, boxes)}px`
+	);
 }
 
 function renderBoxes() {
 	const area = document.getElementById("area");
 	area.innerHTML = "";
-	for (let i = 0; i < boxes; i++) {
-		console.log("box");
+	for (let i = 0; i < boxes; i++)
 		area.appendChild(document.createElement("div"));
-	}
 	document.getElementById("count").innerHTML = boxes;
+	updateTiling();
 }
 
-window.onload = updateTiling;
+window.onload = renderBoxes;
 window.onresize = updateTiling;
