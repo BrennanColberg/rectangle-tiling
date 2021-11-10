d6741a4253bc05932da63a29bc917d017da8484f
Brennan Colberg <brennan.colberg@gmail.com>
2021-11-10 13:07:32 -0800

    v2

--- algorithm.js
+++ algorithm.js
@@ -2,4 +2,10 @@ function v1(longer, shorter, count) {
 	return longer > shorter * count ? shorter : longer / count;
 }
 
-const algorithm = v1;
+function v2(longer, shorter, count) {
+	if (shorter * count < longer) return shorter;
+	if (shorter * count > longer * 2) return shorter / 2;
+	return longer / count;
+}
+
+const algorithm = v2;
