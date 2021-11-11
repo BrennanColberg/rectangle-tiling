b369153c9838b9eda16b00a9baa883e538e2fa0d
Brennan Colberg <brennan.colberg@gmail.com>
2021-11-11 06:30:10 -0800

    comment unused algorithms out

--- algorithm.js
+++ algorithm.js
@@ -1,44 +1,44 @@
-function v1(longer, shorter, count) {
-	return longer > shorter * count ? shorter : longer / count;
-}
+// function v1(longer, shorter, count) {
+// 	return longer > shorter * count ? shorter : longer / count;
+// }
 
-function v2(longer, shorter, count) {
-	if (shorter * count < longer) return shorter;
-	if (shorter * count > longer * 2) return shorter / 2;
-	return longer / count;
-}
+// function v2(longer, shorter, count) {
+// 	if (shorter * count < longer) return shorter;
+// 	if (shorter * count > longer * 2) return shorter / 2;
+// 	return longer / count;
+// }
 
-// helper for later algorithms
-function expensiveOverflows(longer, shorter, count, size) {
-	// simulate putting in `count` boxes
-	let x = 0; // same axis as `longer`
-	let y = 0; // same axis as `shorter`
-	for (let i = 0; i < count; i++) {
-		x += size;
-		// wrap around whenever we overflow on the long side
-		if (x > longer) {
-			x = size;
-			y += size;
-		}
-	}
-	// if we *aren't* overflowing on the short side, this is
-	// the biggest size that fits, so we should return it!
-	return y + size > shorter;
-}
+// // helper for later algorithms
+// function expensiveOverflows(longer, shorter, count, size) {
+// 	// simulate putting in `count` boxes
+// 	let x = 0; // same axis as `longer`
+// 	let y = 0; // same axis as `shorter`
+// 	for (let i = 0; i < count; i++) {
+// 		x += size;
+// 		// wrap around whenever we overflow on the long side
+// 		if (x > longer) {
+// 			x = size;
+// 			y += size;
+// 		}
+// 	}
+// 	// if we *aren't* overflowing on the short side, this is
+// 	// the biggest size that fits, so we should return it!
+// 	return y + size > shorter;
+// }
+
+// function expensive(longer, shorter, count) {
+// 	// shorter side is the largest possible size
+// 	let size = shorter;
+// 	// count down until one size doesn't overflow
+// 	while (!overflows(longer, shorter, count, size)) size--;
+// 	// return the biggest (integer) option that doesn't overflow
+// 	return size;
+// }
 
 function overflows(longer, shorter, count, size) {
 	const boxesPerRow = Math.floor(longer / size);
 	const totalHeight = Math.ceil(count / boxesPerRow) * size;
 	return totalHeight > shorter;
-}
-
-function expensive(longer, shorter, count) {
-	// shorter side is the largest possible size
-	let size = shorter;
-	// count down until one size doesn't overflow
-	while (!overflows(longer, shorter, count, size)) size--;
-	// return the biggest (integer) option that doesn't overflow
-	return size;
 }
 
 // relies on the assumption that above a certain width *all* insertions
